7b2bf86e07a8abd6728877022e8f750f46be2d32
Thomas Intardonato <intardonato@gmail.com>
2021-11-27 01:34:47 -0500

    Added .cpp and .py versions. Added README.md

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -7,11 +7,11 @@
 "use strict"
 
 for(let i = 1; i <= 100; i++){
-    if(i%15 == 0){
+    if(i % 15 == 0){
         console.log("FizzBuzz");
-    }else if(i%3 == 0){
+    }else if(i % 3 == 0){
         console.log("Fizz");
-    }else if(i%5==0){
+    }else if(i % 5==0){
         console.log("Buzz");
     }else{
         console.log(i);
---
+++ FizzBuzz.py
@@ -0,0 +1,14 @@
+
+#import sys
+
+for i in range(1,100):
+    if i % 15 == 0:
+        print("FizzBuzz")
+    else: 
+        if i % 3 == 0:
+            print("Fizz")
+        else: 
+            if i%5==0:
+                print("Buzz")
+            else:
+                print(i)
