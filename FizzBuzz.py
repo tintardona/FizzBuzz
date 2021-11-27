5d8c0df88600b178cec6a2928e0b6114fcfb27a7
Thomas Intardonato <intardonato@gmail.com>
2021-11-27 03:19:39 -0500

    Made FizzBuzz.py into a one-liner using list comprehension

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,14 +1,20 @@
+# Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number 
+# and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”.
 
-#import sys
 
-for i in range(1,100):
+
+
+
+
+for i in range(1, 101):
     if i % 15 == 0:
         print("FizzBuzz")
-    else: 
-        if i % 3 == 0:
-            print("Fizz")
-        else: 
-            if i%5==0:
-                print("Buzz")
-            else:
-                print(i)
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+       print("Buzz")
+    else:
+       print(i)
+
+ 
+print( [ "FizzBuzz" if x % 15 == 0 else "Fizz" if x % 3 == 0 else "Buzz" if x % 5 == 0  else x for x in range(1,101)])
