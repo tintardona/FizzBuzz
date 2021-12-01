c376a411dd320b42005ae9401fbf646cd5e28469
Thomas Intardonato <intardonato@gmail.com>
2021-11-30 21:27:45 -0500

    Added classes to FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,21 +1,69 @@
 /*
 "Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz"."
-*/
 
-package fizzbuzz;
+Concept: make a Fizz class and a Buzz class
+*/
+//package fizzbuzz;
+
 
 public class FizzBuzz{
     public static void main(String[] args){
+        Fizz fizz = new Fizz();
+        Buzz buzz = new Buzz();
+
         for(int i = 1; i <= 100; i++){
             if( i % 15 == 0){
                 System.out.println("FizzBuzz");
-            }else if( i % 3 == 0){
-                System.out.println("Fizz");
-            }else if( i % 5==0){
-                System.out.println("Buzz");
+            }else if( fizz.testFizz(i)){
+                fizz.printFizz();
+            }else if( buzz.testBuzz(i)){
+                buzz.printBuzz();
             }else{
                 System.out.println(i);
             }
         } 
     }
 }
+
+
+
+class Fizz{
+
+    private boolean isFizz = false;
+
+    public Fizz(){
+
+         isFizz = false;
+    }
+
+    public void printFizz(){
+        System.out.println("Fizz");
+    }
+    
+    public boolean testFizz(int i){
+        return (i % 3 == 0);
+    }
+
+}
+
+
+class Buzz{
+
+    private boolean isBuzz = false;
+
+    public Buzz(){
+
+         isBuzz = false;
+    }
+
+    public void printBuzz(){
+        System.out.println("Buzz");
+    }
+    
+    public boolean testBuzz(int i){
+        return (i % 5 == 0);
+    }
+
+}
+
+
