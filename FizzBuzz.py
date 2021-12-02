3ec2e670dd433e869d912aa38f71cf825f919bc0
Thomas Intardonato <intardonato@gmail.com>
2021-12-02 01:58:56 -0500

    Added execution time metrics to FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,7 +1,7 @@
 # Write a program that prints the numbers from 1 to 100. But for multiples of three print "Fizz" instead of the number 
 # and for the multiples of five print "Buzz". For numbers which are multiples of both three and five print "FizzBuzz".
-
-
+import time
+startTime = time.time_ns()
 for i in range(1, 101):
     if i % 15 == 0:
         print("FizzBuzz")
@@ -11,6 +11,10 @@ for i in range(1, 101):
        print("Buzz")
     else:
        print(i)
+print("Elapsed time: ", time.time_ns() - startTime )
 
+startTime = time.time_ns()
  
 [print(("Fizz" * (x % 3==0) + "Buzz" * (x % 5==0)) or x) for x in range(1,101)]
+
+print("Elapsed time: ", time.time_ns() - startTime)
