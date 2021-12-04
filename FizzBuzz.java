78f06f43c11ea6032cc822968d2565d71582a7b4
Thomas Intardonato <intardonato@gmail.com>
2021-12-03 22:19:48 -0500

    Added gui window to FizzBuzz.java

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -12,12 +12,12 @@ import javax.swing.*;
 
 public class FizzBuzz{
 
-    public static void createAndShowGUI() {
+    private static void createAndShowGUI() {
         //Create and set up the window.
         JFrame frame = new JFrame("FizzBuzz");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
-        JLabel emptyLabel = new JLabel("");
+        JLabel emptyLabel = new JLabel("Label");
         emptyLabel.setPreferredSize(new Dimension(600, 500));
         frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
     
